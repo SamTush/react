092f895f0b816e7cb35d0ca6b48df4d2a702ffeb
Samson Tush <samo4421@icloud.com>
2023-04-22 12:52:15 +0300

    Add create react app and linter files

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+const Content = () => (
+  <main>
+    <ul>
+      <li>
+        hello Kevin!
+      </li>
+    </ul>
+  </main>
+);
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header>
+    <h1>
+      Groceries list
+    </h1>
+  </header>
+);
+
+export default Header;
