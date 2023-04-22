0bef680c07422e95ff38c60f59e88e948a33b97e
Samson Tush <samo4421@icloud.com>
2023-04-22 16:55:38 +0300

    Add useState

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,35 @@
-const Content = () => (
-  <main>
-    <ul>
-      <li>
-        hello Kevin!
-      </li>
-    </ul>
-  </main>
-);
+import { useState } from 'react';
+import './Content.scss';
+
+const Content = () => {
+  const [name, setName] = useState('Tim');
+
+  const names = ['Tim', 'Tom', 'Joseph'];
+
+  const handleName = () => {
+    const change = Math.floor(Math.random() * 3);
+    return (
+      setName(names[change])
+    );
+  };
+
+  return (
+    <main>
+      <ul>
+        <li>
+          hello
+          &nbsp;
+          {name}
+          !
+        </li>
+        <li>
+          <button className="btn btn-outline-light mt-4" type="submit" onClick={handleName}>
+            Click me
+          </button>
+        </li>
+      </ul>
+    </main>
+  );
+};
 
 export default Content;
